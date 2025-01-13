9af5d732f71402dac11399ff173972487c5efad5
rxsiebearr <chzou1@s.sfusd.edu>
2025-01-13 14:47:30 -0800

    L4 finished

--- M10.twin_stick_workbook/player.gd
+++ M10.twin_stick_workbook/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var speed := 500.0
 @export var drag_factor := 10.0
-@onready var sprite_2d: Sprite2D = %Sprite2D
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
--- M10.twin_stick_workbook/sprite_2d.gd
+++ M10.twin_stick_workbook/sprite_2d.gd
@@ -8,12 +8,23 @@ const GODOT_UP_RIGHT = preload("res://player/godot_up_right.png")
 
 const UP_LEFT = Vector2.UP + Vector2.LEFT
 const UP_RIGHT = Vector2.UP + Vector2.RIGHT
-const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
 const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
 
 
-
+func _process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction_discrete := direction.sign()
 	match direction_discrete:
 		Vector2.RIGHT, Vector2.LEFT:
-			sprite_2d.texture = GODOT_RIGHT 
+			texture = GODOT_RIGHT 
+		Vector2.UP:
+			texture = GODOT_UP
+		Vector2.DOWN:
+			texture = GODOT_BOTTOM
+		UP_RIGHT, UP_LEFT:
+			texture = GODOT_UP_RIGHT
+		DOWN_RIGHT, DOWN_LEFT:
+			texture = GODOT_BOTTOM_RIGHT
+	if direction_discrete.length() > 0:
+		flip_h = direction_discrete.x < 0.0
