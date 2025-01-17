2cbc25a2ae42391a5a7e689bc7071e32ab7bbfcb
ylirin <xili1@s.sfusd.edu>
2025-01-17 11:15:03 -0800

    L7

---
+++ M10.twin_stick_workbook/weapon_pivot.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var is_using_gamepad := false
+
+@onready var _weapon_anchor: Marker2D = %WeaponAnchor
+
+
+func _process(_delta: float) -> void:
+	var aim_direction := Vector2.ZERO
+	aim_direction = global_position.direction_to(get_global_mouse_position())
+	if aim_direction.length() > 0.1:
+		rotation = aim_direction.angle()
+	z_index = 3
+	if aim_direction.y < 0.0:
+		z_index = 1
